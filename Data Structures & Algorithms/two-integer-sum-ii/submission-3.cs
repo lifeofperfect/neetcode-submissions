aeb6b052f70481dbdc77ad7e3e192b4131199e00
Perfect Igbadumhe <perfect.igbadumhe@gmail.com>
2026-08-01 04:25:42 +0200

    Add: two-integer-sum-ii - submission-3

---
+++ Data Structures & Algorithms/two-integer-sum-ii/submission-3.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        int left = 0;
+        int right = numbers.Length-1;
+
+        int[] result = new int[2];
+
+        while(left < right){
+            int sum = numbers[left] + numbers[right];
+
+            if(sum == target){
+                return new int[] {left + 1, right +1};
+            }
+
+            if(sum < target){
+                left++;
+            }
+
+            if(sum > target){
+                right--;
+            }
+        }
+
+        return new int[] { };
+    }
+}
