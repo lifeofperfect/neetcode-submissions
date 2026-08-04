52f88440367b95597b4fc974848a2a08a52adfd2
Perfect Igbadumhe <perfect.igbadumhe@gmail.com>
2026-08-04 23:27:55 +0200

    Add: max-water-container - submission-0

---
+++ Data Structures & Algorithms/max-water-container/submission-0.cs
@@ -0,0 +1,26 @@
+public class Solution {
+    public int MaxArea(int[] heights) {
+        int left = 0;
+        int right = heights.Length - 1;
+
+        int maxWater = 0;
+
+        while(left < right){
+            int width = right - left;
+
+            int height = Math.Min(heights[left], heights[right]);
+
+            int water = width * height;
+
+            maxWater = Math.Max(maxWater, water);
+
+            if(heights[left] < heights[right]){
+                left++;
+            }else{
+                right--;
+            }
+        }
+
+        return maxWater;
+    }
+}
